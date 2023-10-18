ca3637fcc2352708658f72db58adbbdff3e23786
Kfir Toledo <kfir.toledo@ibm.com>
2023-10-18 14:38:09 +0300

    check timeout

---
+++ pkg/platform/k8s/pods-controller.go
@@ -0,0 +1,18 @@
+package k8s
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type PodReconciler struct {
+	client.Client
+	ipToLabel map[string]map[string]string
+}
+
+func (r *PodReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.Pod{}).
+		Complete(r)
+}
